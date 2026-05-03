6caf204af896a840668fdafc98d2a5191aeb659d
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-05-03 13:37:54 +0300

    add missed exports

--- impl/mariadb-sql-impl/src/main/java/module-info.java
+++ impl/mariadb-sql-impl/src/main/java/module-info.java
@@ -6,4 +6,6 @@ module consulo.sql.impl.mariadb {
     requires consulo.sql.language.api;
     requires consulo.sql.language.impl;
     requires consulo.language.impl;
+
+    exports consulo.sql.language.impl.mariadb;
 }
--- impl/postgresql-sql-impl/src/main/java/module-info.java
+++ impl/postgresql-sql-impl/src/main/java/module-info.java
@@ -6,4 +6,6 @@ module consulo.sql.impl.postgresql {
     requires consulo.sql.language.api;
     requires consulo.sql.language.impl;
     requires consulo.language.impl;
+
+    exports consulo.sql.language.impl.postgresql;
 }
